502d2b4fb891e814b17f8cff3aee6f04e61b61d1
Kizito Njoku <nkizito57@gmail.com>
2019-11-06 04:20:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 def countdown
   number = 10
-  while number >= 0 
+  while number >= 1 
     puts "#{number} SECOND(S)!"
     number -= 1
-   if number == 0 
+   if number == 1 
     puts "HAPPY NEW YEAR!"
    end
   end
